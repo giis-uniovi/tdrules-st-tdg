39785aa84b430df9c47c4a374f02b6e895e61a37
Javier <10879637+javiertuya@users.noreply.github.com>
2025-05-20 17:35:45 +0200

    Refactor BasePetstoreEval to reuse the test module

--- st-tdg-eval/src/test/java/test4giis/tdrules/tdg/st/eval/petstore/BasePetstoreEval.java
+++ st-tdg-eval/src/test/java/test4giis/tdrules/tdg/st/eval/petstore/BasePetstoreEval.java
@@ -1,12 +1,5 @@
 package test4giis.tdrules.tdg.st.eval.petstore;
 
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.rules.TestName;
-
-import giis.tdrules.client.TdRulesApi;
-import giis.tdrules.client.oa.OaSchemaApi;
-import giis.tdrules.client.oa.OaSchemaIdResolver;
 import giis.tdrules.openapi.model.TdSchema;
 import giis.tdrules.store.loader.DataLoader;
 import giis.tdrules.store.loader.gen.DictionaryAttrGen;
@@ -18,33 +11,24 @@ import giis.visualassert.VisualAssert;
 import giis.visualassert.portable.FileUtil;
 import in2test.application.qagrow.QAGrowApiProcess;
 import lombok.extern.slf4j.Slf4j;
+import test4giis.tdrules.tdg.st.test.petstore.BasePetstore;
 
 /**
  * Utilities and creation of the main objects to generate, load and evaluate the petstore
  * Test Data Generation (TDG)
  */
 @Slf4j
-public class BasePetstoreEval {
-	protected static final String PETSTORE_SCHEMA_LOCAL = "../sut-petstore/src/main/resources/openapi.yaml";
+public class BasePetstoreEval extends BasePetstore {
 
 	protected static TdSchema model; // readonly, created before all tests
 
-	@Rule
-	public TestName testName = new TestName();
-
-	@Before
-	public void setUp() {
-		log.info("****** Running test: {} ******", testName.getMethodName());
+	@Override
+	protected boolean isLiveBackend() {
+		return false; // this eval runs a custom mock controller
 	}
-
+	
 	protected void load(ApiWriter writer, String query) {
-		// Configure the schema id resolver to use id attribute as uid.
-		// The exception is the Tag entity, that has an id attribute,
-		// but looking at the source code, a post inserts unconditionally,
-		// allowing repeated id values. This is considered as a non uid.
-		OaSchemaApi schemaApi=new OaSchemaApi(PETSTORE_SCHEMA_LOCAL)
-				.setIdResolver(new OaSchemaIdResolver().setIdName("id").excludeEntity("Tag").excludeEntity("Order0"));
-		model = schemaApi.getSchema();
+		model = getSchema();
 		
 		// Check the schema, because their changes may invalidate all results
 		String modelStr = new giis.tdrules.model.io.ModelJsonSerializer().serialize(model, true);
@@ -56,34 +40,19 @@ public class BasePetstoreEval {
 		// The path resolver is created using a controller mock (writer)
 		// and the data loader using a dictionary
 		DataLoader loader = new DataLoader(model, new OaLiveAdapter("").setApiWriter(writer))
-				.setAttrGen(getPetstoreDictionary().setMinYear(2024));
+				.setAttrGen(getPetstoreEvalDictionary().setMinYear(2024));
 
 		// Generation and loading: Each test first delete all data previous to the generation and load
 		writer.delete("/test/deleteAll");
-		QAGrowApiProcess qagrow = new QAGrowApiProcess(model, new TdRulesApi().setCache("../.tdrules-cache"), loader);
+		QAGrowApiProcess qagrow = new QAGrowApiProcess(model, getRulesApi(), loader);
 		qagrow.genData4ApiQuery(query);
 	}
 
 	/**
-	 * Dictionary to load more user friendly petstore data, includes values enumertions for strings and masks
+	 * Same dictionary, but constraining the interval of order.quantity
 	 */
-	protected IAttrGen getPetstoreDictionary() {
-		return new DictionaryAttrGen()
-				//https://www.southernliving.com/most-popular-pet-names-rover-6829769
-				.with("Pet", "name").dictionary("Max", "Luna", "Charlie", "Bella", "Cooper", "Daisy", "Milo", "Lucy")
-				.with("Pet_photoUrls_xa", "photoUrls").padLeft('0', 6).mask("http://localhost/photos/{}.jpg")
-				.with("Pet_Tags_xa", "name").dictionary("Puppy", "Young", "Old")
-				.with("Category", "name").dictionary("Tiger", "Lion", "Monkey", "Snake")
-				.with("Customer_address_xa", "street").dictionary("Main St", "Broadway", "Park Ave", "Fulton St", "Madison Ave", "Pine St", "Amsterdam Ave", "Wall St")
-				//https://www.ssa.gov/oact/babynames/decades/century.html
-				//https://www.al.com/news/2019/10/50-most-common-last-names-in-america.html
-				.with("Customer", "username").dictionary("James Smith", "Mary Johnson", "Robert Williams", "Patricia Brown", "David Garcia", "Elizabeth Miller", "William Davis", "Barbara Wilson")
-				//https://www.worldatlas.com/articles/most-common-town-and-city-names-in-the-u-s-a.html#:~:text=Washington,this%20way%20is%20no%20surprise.
-				.with("Customer_address_xa", "city").dictionary("Springfield", "Franklin", "Greenville", "Bristol", "Clinton", "Fairview", "Salem", "Madison")
-				.with("Customer_address_xa", "state").dictionary("California", "Texas", "Florida", "New York", "Pennsylvania", "Illinois", "Ohio", "Georgia")
-				.with("Customer_address_xa", "zip").padLeft('0', 6)
-				.with("Order", "quantity").setInterval(1, 13)
-				;
+	protected IAttrGen getPetstoreEvalDictionary() {
+		return ((DictionaryAttrGen)getDictionaryAttrGen()).with("Order", "quantity").setInterval(1, 13);
 	}
 
 }
