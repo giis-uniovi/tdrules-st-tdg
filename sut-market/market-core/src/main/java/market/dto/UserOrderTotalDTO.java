750f4075c070fa65ff76fb931bc55bd09ce06789
María José Suárez-Cabal <70757566+cabaluniovi@users.noreply.github.com>
2026-03-17 15:48:17 +0100

    Establishing the order of properties (#177)

--- sut-market/market-core/src/main/java/market/dto/UserOrderTotalDTO.java
+++ sut-market/market-core/src/main/java/market/dto/UserOrderTotalDTO.java
@@ -1,7 +1,9 @@
 package market.dto;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 // testing: add new class to get the total costs of an user's order
+@JsonPropertyOrder({"count","email","totalAmount"})
 public interface UserOrderTotalDTO {
     String getEmail();
     int getCount();
